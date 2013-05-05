67a0f9cb6e8894e5a277125a9a9d5c61caea0c0d
tihuang <tiffany.huang08@gmail.com>
2013-05-04 23:27:38 -0400

    improved fire rating to use fontawesome vector fonts. is now stars. favorites will be hearts.

--- comboForm.js
+++ comboForm.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var rating = 0;
+
 	$("#metaDataForm").submit(function(e){
 		e.preventDefault();
 		$(location).attr('href','./joystick.html');	
@@ -11,27 +13,56 @@ $(document).ready(function() {
         $("#fireRating").addClass(numStars.toString()).addClass("star-rating");
     });
 
-    $("div.star-rating > s").hover(
-    	 function () {
- 			var numStars = parseInt($(this)[0].className[0])+1;
- 			setStars(numStars);
- 	 	},
-  		function () {
- 			var num = $("#fireRating").attr("class")[0];
- 			setStars(num)
+    // $("div.star-rating > s").hover(
+    	 // function () {
+ 			// var numStars = parseInt($(this)[0].className[0])+1;
+ 			// setStars(numStars);
+ 	 	// },
+  		// function () {
+ 			// var num = $("#fireRating").attr("class")[0];
+ 			// setStars(num)
+		// }
+	// );
+	
+	$('#fireRating').hover(
+		function() {
+			$(this).addClass('hover');
+		},
+		function() {
+			$(this).removeClass('hover');
+			highlight(rating);
 		}
-);
+	);
+	
+	$('#fireRating > i').hover(
+		function() {
+			var num = $('#fireRating > i').index(this);
+			highlight(num);
+			
+		}
+		
+		// function() {
+			// var num = $('#fireRating > i').index(this);
+			// highlight(rating);
+		// }
+	);
+	
+	$('#fireRating > i').click(function() {
+		var num = $('#fireRating > i').index(this);
+		rating = num;
+		
+	});
+	
 
 });
 
-var setStars = function(num){
-        var par = $("#fireRating").children();
+var highlight = function(num) {
+	$('#fireRating').children().each(function(idx, elt) {
+		if (idx <= num) {
+			$(this).removeClass('icon-star-empty').addClass('icon-star');
+		} else {
+			$(this).removeClass('icon-star').addClass('icon-star-empty');
+		}
+	});
+};
 
-        for (var st = 0; st < num; st++){
-        	par[st].className=st+" icon-fire";
-        }
-
-        for (var ar = num; ar < 5; ar++){
-        	par[ar].className=ar+" icon-fire icon-white";
-        }
-}
